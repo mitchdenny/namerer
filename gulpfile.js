5ce8492a01d6420f801d24342dd17ab18d9ca0e6
Mitch Denny <mitch@mitchdenny.com>
2015-12-21 15:18:52 +1100

    Updated gulp to output a ##vso message.

--- gulpfile.js
+++ gulpfile.js
@@ -11,39 +11,49 @@ var paths = {
 	metadata: ['**/package.json', '!node_modules/**/package.json']
 }
 
-gulp.task('compile', function() {
+gulp.task('compile', function () {
 	return gulp
 		.src(paths.typescript)
 		.pipe(ts(tsconfig.compilerOptions))
 		.pipe(gulp.dest('.'));
 });
 
-gulp.task('stamp', function(callback) {
-	exec('git describe --abbrev=0', function(err, stdout, stderr) {
-		var originalContents = fs.readFileSync('package.json', 'utf8');
-		var pkg = JSON.parse(originalContents);
+function updatePackage(version) {
+	var originalContents = fs.readFileSync('package.json', 'utf8');
+	var pkg = JSON.parse(originalContents);
 
-		pkg.version = stdout.toString().split('\n')[0];
+	pkg.version = version;
 
-		var updatedContents = JSON.stringify(pkg, null, '\t');
-		fs.writeFileSync('package.json', updatedContents);
+	var updatedContents = JSON.stringify(pkg, null, '\t');
+	fs.writeFileSync('package.json', updatedContents);
+}
+
+function exportVariableToBuildServer(version) {
+	console.log('##vso[task.setvariable variable=Build.SemVer;]%s', version);
+}
+
+gulp.task('stamp', function (callback) {
+	exec('git describe --abbrev=0', function (err, stdout, stderr) {
+		var version = stdout.toString().split('\n')[0];
+		updatePackage(version);
+		exportVariableToBuildServer(version);
 		callback(err);
 	});
 });
 
-gulp.task('pack', function(callback) {
-	exec('npm pack', function(err, stdout, stderr) {
+gulp.task('pack', function (callback) {
+	exec('npm pack', function (err, stdout, stderr) {
 		console.log(stdout);
 		console.log(stderr);
 		callback(err);
 	});
 });
 
-gulp.task('watch', ['default'], function() {
+gulp.task('watch', ['default'], function () {
 	gulp.watch(paths.metadata, ['pack']);
 	gulp.watch(paths.bin, ['pack']);
 	gulp.watch(paths.javascript, ['pack']);
-	gulp.watch(paths.typescript, ['compile']);	
+	gulp.watch(paths.typescript, ['compile']);
 });
 
 gulp.task('default', ['stamp', 'compile', 'pack']);
