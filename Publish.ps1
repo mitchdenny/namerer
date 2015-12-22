d14701f66427fd2b148c3f0b1d7f240d8a158398
Mitch Denny <mitch@mitchdenny.com>
2015-12-22 16:47:12 +1100

    Removed PowerShell hack.

--- Publish.ps1
+++
@@ -1,8 +0,0 @@
-function Publish-Package
-{
-	[CmdletBinding()]
-	param($npmusername, $npmpassword, $npmemail)
-	
-	$setup = "$npmusername`r`n$npmpassword`r`n$npmemail`r`n"
-	$setup | npm adduser
-}
