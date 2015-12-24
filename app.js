a207eb03489eac4ee22e1af05d61069cdc78aafc
Mitch Denny <mitch@mitchdenny.com>
2015-12-24 15:07:00 +1000

    Basic implementation of templates. Breaks existing placeholders.

--- app.js
+++ app.js
@@ -1,47 +1,42 @@
 /// <reference path="../typings/node/node.d.ts" />
+/// <reference path="../typings/underscore/underscore.d.ts" />
 var fs = require('fs');
+var underscore = require('underscore');
 function getVersion() {
     var content = fs.readFileSync(__dirname + '/package.json', 'utf8');
     var pkg = JSON.parse(content);
     return pkg.version;
 }
 exports.getVersion = getVersion;
-function selectRandom(possibilities) {
-    var randomIndex = Math.floor(Math.random() * possibilities.length);
-    var selection = possibilities[randomIndex];
-    return selection;
-}
-function appendLetter(input, alphabet) {
-    var character = selectRandom(alphabet.split(''));
-    var output = input + character;
-    return output;
-}
-function appendActual(input, actual) {
-    var output = input + actual;
-    return output;
-}
-function appendNumber(input, numbers) {
-    var number = selectRandom(numbers.split(''));
-    var output = input + number;
-    return output;
-}
-function generateName(template, alphabet, numbers) {
-    var generators = [];
-    var output = '';
-    for (var templateIndex = 0; templateIndex < template.length; templateIndex++) {
-        var templateCharacter = template[templateIndex];
-        switch (templateCharacter) {
-            case '?':
-                output = appendLetter(output, alphabet);
-                break;
-            case '#':
-                output = appendNumber(output, numbers);
-                break;
-            default:
-                output = appendActual(output, templateCharacter);
-                break;
-        }
+var GeneratorContext = (function () {
+    function GeneratorContext(template, alphabet, numbers) {
+        this.template = template;
+        this.alphabet = alphabet;
+        this.numbers = numbers;
     }
+    GeneratorContext.prototype.selectRandom = function (possibilities) {
+        var randomIndex = Math.floor(Math.random() * possibilities.length);
+        var selection = possibilities[randomIndex];
+        return selection;
+    };
+    GeneratorContext.prototype.actual = function (input) {
+        return input;
+    };
+    GeneratorContext.prototype.alpha = function () {
+        var character = this.selectRandom(this.alphabet);
+        return character;
+    };
+    GeneratorContext.prototype.numeric = function () {
+        var number = this.selectRandom(this.numbers);
+        return number;
+    };
+    return GeneratorContext;
+})();
+function generateName(context) {
+    var generator = underscore.template(context.template, {
+        interpolate: /\[(.+?)\]/g
+    });
+    var output = generator(context);
     return output;
 }
 function generate(template, alphabet, numbers, count) {
@@ -58,8 +53,9 @@ function generate(template, alphabet, numbers, count) {
         count = 1;
     }
     var names = [];
+    var context = new GeneratorContext(template, alphabet.split(''), numbers.split(''));
     for (var nameIndex = 0; nameIndex < count; nameIndex++) {
-        var name_1 = generateName(template, alphabet, numbers);
+        var name_1 = generateName(context);
         names.push(name_1);
         console.log(name_1);
     }
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 /// <reference path="../typings/node/node.d.ts" />
+/// <reference path="../typings/underscore/underscore.d.ts" />
 import * as fs from 'fs';
+import underscore = require('underscore');
 
 export function getVersion() {
 	let content = fs.readFileSync(__dirname + '/package.json', 'utf8');
@@ -7,50 +9,45 @@ export function getVersion() {
 	return pkg.version;
 }
 
-function selectRandom<T>(possibilities: T[]): T {
-	let randomIndex = Math.floor(Math.random() * possibilities.length);
-	let selection = possibilities[randomIndex];
-	return selection;
-}
-
-function appendLetter(input: string, alphabet: string): string {
-	let character = selectRandom(alphabet.split(''));	
-	let output = input + character;
-	return output;
-}
-
-function appendActual(input: string, actual: string): string {
-	let output = input + actual;
-	return output;
-}
-
-function appendNumber(input: string, numbers: string): string {
-	let number = selectRandom(numbers.split(''));	
-	let output = input + number;
-	return output;
-}
-
-function generateName(template: string, alphabet: string, numbers: string): string {
-	let generators: ((() => string) | string)[] = [];
-	
-	let output: string = '';
-	
-	for(let templateIndex = 0; templateIndex < template.length; templateIndex++) {
-		let templateCharacter = template[templateIndex];
-
-		switch (templateCharacter) {
-			case '?':
-				output = appendLetter(output, alphabet);
-				break;
-			case '#':
-				output = appendNumber(output, numbers);
-				break;
-			default:
-				output = appendActual(output, templateCharacter);
-				break;
-		}
+class GeneratorContext {
+	constructor(template: string, alphabet: string[], numbers: string[]) {
+		this.template = template;
+		this.alphabet = alphabet;
+		this.numbers = numbers;
 	}
 	
+	public template: string;
+
+	private alphabet: string[];
+	private numbers: string[];
+	
+	private selectRandom<T>(possibilities: T[]): T {
+		let randomIndex = Math.floor(Math.random() * possibilities.length);
+		let selection = possibilities[randomIndex];
+		return selection;
+	}
+	
+	public actual(input: string): string {
+		return input;
+	}
+	
+	public alpha(): string {
+		let character = this.selectRandom(this.alphabet);
+		return character;
+	}
+	
+	public numeric(): string {
+		let number = this.selectRandom(this.numbers);
+		return number;
+	}
+}
+
+function generateName(context: GeneratorContext): string {
+	let generator = underscore.template(context.template, {
+		interpolate: /\[(.+?)\]/g
+	});
+	
+	let output = generator(context);
 	return output;
 }
 
@@ -73,8 +70,14 @@ export function generate(template?: string, alphabet?: string, numbers?: string,
 	
 	let names: string[] = [];
 	
+	let context = new GeneratorContext(
+		template,
+		alphabet.split(''),
+		numbers.split('')	
+	);
+	
 	for (let nameIndex = 0; nameIndex < count; nameIndex++) {
-		let name = generateName(template, alphabet, numbers);
+		let name = generateName(context);
 		names.push(name);
 		console.log(name);
 	}
