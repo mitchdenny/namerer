b9f547976e62ff123459640e1999f9ac6d3e39d4
Mitch Denny <mitch@mitchdenny.com>
2016-01-04 17:05:55 +1100

    Added doc generation to the build.

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var paths = {
 	bin: 'bin/**/*',
 	typescript: 'src/**/*.ts',
 	javascript: '**/*.js',
+	docs: 'docs/**/*',
 	metadata: ['**/package.json', '!node_modules/**/package.json', '**/tsconfig.json', '**/jsconfig.json']
 }
 
@@ -51,11 +52,20 @@ gulp.task('pack', function (callback) {
 	});
 });
 
+gulp.task('docs', function (callback) {
+	exec('make -C docs html', function(err, stdout, stderr) {
+		console.log(stdout);
+		console.log(stderr);
+		callback(err);
+	});
+});
+
 gulp.task('watch', ['default'], function () {
 	gulp.watch(paths.metadata, ['pack']);
 	gulp.watch(paths.bin, ['pack']);
 	gulp.watch(paths.javascript, ['pack']);
 	gulp.watch(paths.typescript, ['compile']);
+	gulp.watch(paths.docs, ['docs']);
 });
 
-gulp.task('default', ['stamp', 'compile', 'pack']);
+gulp.task('default', ['stamp', 'compile', 'pack', 'docs']);
